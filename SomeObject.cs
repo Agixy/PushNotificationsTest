b053f9174470aaf24d4cdf5e6edfde93c7966ae4
Aga <a.glowacka224@gmail.com>
2020-11-19 19:27:01 +0100

    First commit

---
+++ SomeObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace PushNotificationsTest
+{
+    public class SomeObject
+    {
+        private readonly HttpClient httpClient;
+        public int Id { get; set; }
+
+        public SomeObject(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<int> PlaceOrder(SomeObject someObject)
+        {
+            var response = await httpClient.PostAsJsonAsync("objects", someObject);
+            response.EnsureSuccessStatusCode();
+            var orderId = await response.Content.ReadFromJsonAsync<int>();
+            return orderId;
+        }
+
+        public async Task SubscribeToNotifications(NotificationSubscription subscription)
+        {
+            var response = await httpClient.PutAsJsonAsync("notifications/subscribe", subscription);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
